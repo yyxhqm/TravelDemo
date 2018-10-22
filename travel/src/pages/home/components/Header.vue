b24775be375de45dc2f7f76a0d9f8d5fc8557408
YuanYongXiao <2846404212@qq.com>
2018-10-22 10:03:07 +0800

    city router

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -6,10 +6,13 @@
  </div>
  <div class="header-input">
    <span class="iconfont">&#xe632;</span>
-     输入城市/景点/游玩主题</div>
- <div class="header-right arrow-iocn">{{this.city}}
-   <span class="iconfont">&#xe6aa;</span>
+     输入城市/景点/游玩主题
  </div>
+ <router-link to='/city'>
+ <div class="header-right">{{this.city}}
+   <span class="iconfont arrow-iocn">&#xe6aa;</span>
+ </div>
+ </router-link>
 </div>
 </template>
 
@@ -28,7 +31,7 @@ export default {
 @import '~styles/varibles.styl';
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -51,6 +54,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arrow-iocn{
         margin-left: -0.4rem
       }
